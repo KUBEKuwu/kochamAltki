5512682e2803c992001559ebc071fc9be98ed715
KUBEKuwu <118252003+KUBEKuwu@users.noreply.github.com>
2023-11-26 19:07:45 +0100

    Add files via upload

---
+++ dziesietneNaBinarne.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <bitset>
+
+int main() {
+    int liczba;
+    std::cout << "Podaj liczbe w systemie dziesietnym: ";
+    std::cin >> liczba;
+
+    std::bitset<sizeof(int) * 8> binarna(liczba);
+    std::cout << "Liczba w systemie binarnym: " << binarna << std::endl;
+
+    return 0;
+}
