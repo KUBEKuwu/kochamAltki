d611ce37fbb61137915e96634b9fbdcf65e978f1
KUBEKuwu <118252003+KUBEKuwu@users.noreply.github.com>
2023-11-26 22:20:42 +0100

    Add files via upload

---
+++ Silniaa.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+int silnia(int n) {
+    if (n == 0) {
+        return 1;
+    }
+
+    return n * silnia(n - 1);
+}
+
+int main() {
+    int liczba;
+
+    std::cout << "Podaj liczbe do obliczenia silni: ";
+    std::cin >> liczba;
+
+    if (liczba < 0) {
+        std::cout << "Silnia jest zdefiniowana tylko dla liczb nieujemnych." << std::endl;
+    }
+    else {
+        int wynik = silnia(liczba);
+        std::cout << "Silnia liczby " << liczba << " to: " << wynik << std::endl;
+    }
+
+    return 0;
+}
